af8a8fc6703ff921ca2b3a1d51337d594764299d
jasand86 <joshs@2600hz.com>
2017-05-15 12:24:03 -0700

    flexbox initial implementation

--- demo/js/script.js
+++ demo/js/script.js
@@ -35,17 +35,3 @@ $(document).ready(function() {
 
 
 });
-
-// triggers animations when objects hits a certain distance (px) from the top of window
-
-// $(window).scroll(function() {
-// 	$('.start-anim').each(function(){
-// 		var imagePos = $(this).offset().top;
-// 		var topOfWindow = $(window).scrollTop();
-
-// 		if (imagePos < topOfWindow+500) {
-// 			$(this).children('.left').addClass("animated fadeInLeft");
-// 			$(this).children('.right').addClass("animated fadeInRight");
-// 		}
-// 	});
-// });
