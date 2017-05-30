af7e86168d439ed839347b786b24526ebbf62471
jasand86 <joshs@2600hz.com>
2017-05-29 17:12:05 -0700

    Docs -> Column Syntax work

--- docs/js/script.js
+++ docs/js/script.js
@@ -8,58 +8,29 @@ $(document).ready(function() {
 	    	targetCode = $('.js-varyColumns--code'),
 			newCode = '<div><span class="indent"></span>&lt;div class="grid__col"&gt;&lt;/div&gt;<br></div>';
 
-	    if (columns.length >= 2 && columns.length != 12) {
+	    if (columns.length >= 2 && columns.length < 12) {
 	        targetRow.append(newColumn);
 	        targetCode.children('.target').append(newCode);
 	    }
-	    // else if ($columns.length == 12) {
-	    // 	// throw a warning here
-	    // }
+
+	    else if (columns.length == 12) {
+	    	alert("While it's possible to add as many columns as you want, it is recommended to stick with 12 columns. If you want smaller divisions, you can always nest columns.");
+	    }
 	});
 
 	    
 	$('.js-btn--removeCol').on('click', function(removeCol) {
+
 		var targetRow = $('.js-varyColumns--row'),
 	    	columns = targetRow.children('.grid__col'),
-
 	    	targetCode = $('.js-varyColumns--code'),
 			newCode = '<span class="indent"></span>&lt;div class="grid__col"&gt;&lt;/div&gt;<br>';
 
-	    	// existingColumn = $('.grid__col');
-
 		if (columns.length <= 12 && columns.length != 2) {
-			// console.log('remove btn is clicked');
-
 	        targetRow.children().last().remove();
 	        targetCode.children('.target').children('div').last().remove();
 	    }
 
 	});
-
-
-
-		// function moveButton(elem){
-		//     var $selected = $('#selected');
-		//     var $kids = $selected.children();
-		//     if ($kids.length >= 3)
-		//     {
-		//         $kids.last().remove();
-		//     }
-		//     $(elem).detach().appendTo('#selected');
-		// }
-
-
-
-		// $( document.body ).on('click', function() {
-		// 	$( document.body ).append( $( "<div>" ) );
-		// 	var n = $( "div" ).length;
-		// 	$( "span" ).text( "There are " + n + " divs." +
-		// 	  "Click to add more.");
-		// });
-	
-
-
-
-
 
 });
